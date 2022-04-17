da01ef96eb525bf32483ed60fbd5e0f8a55fb565
hayun <102849597+hmogae@users.noreply.github.com>
2022-04-17 19:51:37 +0900

    20220417 메세지 업데이트

--- input_str.c
+++ input_str.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  char str[16];
-  printf("문자열을 입력해!(최대 15자 까지야!): ");
+	char str[16];
+	printf("문자열을 입력해주세요!(최대 15자): ");
 	scanf("%s", str);
-	printf("입력한 문자열은 \"%s\"이야!\n", str);
+	printf("입력한 문자열은 \"%s\"입니다!\n", str);
 	return 0;
 }
