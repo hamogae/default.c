686b826194eca52b39f69ca8d1c1c6a3f74df485
hayun <102849597+hmogae@users.noreply.github.com>
2022-04-17 19:57:47 +0900

    Create control_character.c

---
+++ default/control_character.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	printf("\"hello\"");
+	return 0;
+} // 큰따움표
+
+int main2() {
+	printf("Hello!\ngg!");
+	return 0;
+} // 줄바꿈
+
+int main3() {
+	printf("\tHello!");
+	return 0;
+} // 탭
+
+int main() {
+	printf("\'Hello!\'");
+	return 0;
+} // 작은 따움표
+
+int main() {
+	printf("\\Hello!\\");
+	return 0;
+} // 역슬레시
